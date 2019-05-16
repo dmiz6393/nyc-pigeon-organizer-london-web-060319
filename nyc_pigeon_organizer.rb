e8799b4e51a9ef43ef80c360f19251da3e98ce7a
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-16 12:31:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,6 @@
 def nyc_pigeon_organizer(data)
   new_hash={} 
   data.each do |color,color_name| 
-    color_name.each do |colors,name|  
+    color_name.each do |colors,name| 
+      name.each do |names|
 end
