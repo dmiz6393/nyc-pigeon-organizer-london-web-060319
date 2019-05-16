41b813d70bb266e2ff26dd6600aa7dbfd866ad8c
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-16 13:13:04 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+def nyc_pigeon_organizer(pigeon_data)
+  new_hash={} 
+ pigeon_data.each do |type_data,data| 
+    data.each do |more_info, name| 
+      name.each do |names|
+        if new_hash.has_key?(names)
+     if new_hash[names].has_key?(type_data)
+       
+new_hash[names][type_data] << more_info.to_s
+
+else 
+  new_hash[names][type_data] = [more_info.to_s]
+          end
+        else
+          new_hash[names] = {type_data => [more_info.to_s]} 
+        end
+      end
+    end
+  end
+ new_hash
 end
